cb4ac12a3aa94fc0412ae82ec85c29d67f30d8fd
FubukiSato <satofubuki0510@gmail.com>
2021-10-20 22:59:55 +0900

    first_commit

---
+++ ex04/Date.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "Date.h"
+
+void Date::set(int m, int d, int y) {}
+
+void Date::print() {}
+
+int Date::get_month() {}
+
+int Date::get_year() {}
+
+int Date::get_day() {}
+
+void Date::inc_day() {}
+
+void Date::inc_month() {}
+
+void Date::inc_year() {}
