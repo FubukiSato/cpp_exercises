a205aae6f942363514ea8b5c5301e5ace3dd9bfd
FubukiSato <satofubuki0510@gmail.com>
2021-10-30 16:22:27 +0900

    edit

---
+++ ex06/Circle.cpp
@@ -0,0 +1,25 @@
+#include "Circle.h"
+
+std::string Circle::get_name() const{
+
+}
+
+float Circle::compute_area() const{
+
+}
+
+Circle* Circle::create() const{
+
+}
+
+Circle* Circle::clone() const{
+
+}
+
+Circle::Circle(Point2D center, float radius){
+    
+}
+    
+Circle::~Circle(){
+
+}
---
+++ ex06/Rectangle.cpp
@@ -0,0 +1,25 @@
+#include "Rectangle.h"
+
+std::string Rectangle::get_name() const{
+
+}
+
+float Rectangle::compute_area() const{
+
+}
+
+Rectangle* Rectangle::create() const{
+
+}
+
+Rectangle* Rectangle::clone() const{
+
+}
+
+Rectangle::Rectangle(Point2D left_corner, float width, float height){
+    left_corner = left_corner;
+}
+    
+Rectangle::~Rectangle(){
+
+}
