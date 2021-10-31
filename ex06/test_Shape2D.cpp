9987733b34188c66ddbfdb84af335ebf70a3d66c
FubukiSato <satofubuki0510@gmail.com>
2021-10-31 16:18:15 +0900

    edit

--- ex06/test_Shape2D.cpp
+++ ex06/test_Shape2D.cpp
@@ -5,7 +5,7 @@
 int main(void) {
   // COMPLETE 
   // Write tests to test your implementation of the classes Rectangle and Circle. 
-  Rectangle a;
+  Rectangle rec = Rectangle()
   Point2D point2d_rec;
   point2d_rec.x = 3.0; point2d_rec.y = 5.0;
   Rectangle::create(point2d_rec,4.0,5.0);
