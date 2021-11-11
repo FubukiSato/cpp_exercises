f686b5e1fb44642c2ee527c3bb0b8bf91f86f53f
FubukiSato <satofubuki0510@gmail.com>
2021-11-11 16:52:49 +0900

    edit

---
+++ ex08/Mat4x4.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include "Mat4x4.h"
+
+Mat4x4& Mat4x4::operator+ (const Mat4x4& other){
+
+}
+
+Mat4x4& Mat4x4::operator- (const Mat4x4& other){
+
+}
+
+Mat4x4& Mat4x4::operator* (const Mat4x4& other){
+
+}
+
+Mat4x4& Mat4x4::operator+= (const Mat4x4& other){
+
+}
+
+Mat4x4& Mat4x4::operator-= (const Mat4x4& other){
+
+}
+
+Mat4x4& Mat4x4::operator*= (const Mat4x4& other){
+
+}
+
+Mat4x4& Mat4x4::operator- (){
+
+}
+
+bool Mat4x4::operator== (const int a){
+
+}
+
+bool Mat4x4::operator== (const Mat4x4 a){
+
+}
+
+double& Mat4x4::operator()(int i, int j){
+
+}
+
+ostream& operator<< (ostream& os, const Mat4x4& other){
+
+}
+
+Mat4x4::Mat4x4(){
+
+}
+
+Mat4x4::Mat4x4(double mat[4][4]){
+
+}
