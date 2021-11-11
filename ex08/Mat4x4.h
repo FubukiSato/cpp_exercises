58730d92bd75be8901d70e41d0b2ae74c1c1682f
FubukiSato <satofubuki0510@gmail.com>
2021-11-11 16:03:10 +0900

    edit

---
+++ ex08/Mat4x4.h
@@ -0,0 +1,21 @@
+#ifndef Mat4x4_H
+#define Mat4x4_H
+
+#include <iostream>
+using namespace std;
+
+class Mat4x4{
+    public:
+    Mat4x4& operator+ (const Mat4x4& other);
+    Mat4x4& operator- (const Mat4x4& other);
+    Mat4x4& operator* (const Mat4x4& other);
+    Mat4x4& operator+= (const Mat4x4& other);
+    Mat4x4& operator-= (const Mat4x4& other);
+    Mat4x4& operator*= (const Mat4x4& other);
+    Mat4x4& operator- ();
+    bool operator== (const int a);
+    double& operator()(int i, int j);
+    friend ostream& operator<< (ostream& os, const Mat4x4& other);
+};
+
+#endif
