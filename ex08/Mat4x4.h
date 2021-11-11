486d9e16dfd3e4444ada2a4d814f9865c59cd72c
FubukiSato <satofubuki0510@gmail.com>
2021-11-11 16:34:56 +0900

    edit

--- ex08/Mat4x4.h
+++ ex08/Mat4x4.h
@@ -14,8 +14,13 @@ class Mat4x4{
     Mat4x4& operator*= (const Mat4x4& other);
     Mat4x4& operator- ();
     bool operator== (const int a);
+    bool operator== (const Mat4x4);
     double& operator()(int i, int j);
     friend ostream& operator<< (ostream& os, const Mat4x4& other);
+    
+
+    Mat4x4();
+    Mat4x4(double mat[4][4]);
 };
 
 #endif
