32ee706260428eede9958cc907c8fe4e8d7f4007
FubukiSato <satofubuki0510@gmail.com>
2021-11-25 23:38:58 +0900

    edit

---
+++ ex12/priority_queue.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <queue>
+
+struct Point {
+ Point() : x(0.0), y(0.0), z(0.0) {}
+ Point(double x, double y, double z) : x(x), y(y), z(z) {}
+ double x, y, z;
+};
+
+
+int main(void) {
+  // COMPLETE 
+  // Add code to test your implementation of the function find_k_closest()
+}
