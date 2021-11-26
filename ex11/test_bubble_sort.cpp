7900506088109f5cf5ef76124652d5ec4fa44d8c
FubukiSato <satofubuki0510@gmail.com>
2021-11-27 01:31:38 +0900

    edit

--- ex11/test_bubble_sort.cpp
+++ ex11/test_bubble_sort.cpp
@@ -12,9 +12,13 @@ namespace PL4 {
 
 int main(){
 
-vector<int> vec{11,25,4,555,67,3,1};
-vector<int>::iterator itr1 = vec.begin();
-vector<int>::iterator itr2 = vec.end();
+vector<int> vecint{11,25,4,555,67,3,1};
+vector<int>::iterator itr1 = vecint.begin();
+vector<int>::iterator itr2 = vecint.end();
+
+vector<double> vecdouble{1.3,25.4,4.44,555.21,67.1,3.22,1.1};
+vector<double>::iterator itr3 = vecdouble.begin();
+vector<double>::iterator itr4 = vecdouble.end();
 
 PL4::sort(itr1,itr2);
 
@@ -22,6 +26,16 @@ while(itr1 != itr2){
     cout<<*itr1 <<" "<<endl;
     *itr1++;
 }
+
+cout<<""<<endl;
+
+PL4::sort(itr3,itr4);
+
+while(itr3 != itr4){
+    cout<<*itr3 <<" "<<endl;
+    *itr3++;
+}
+
 }
 
 namespace PL4 {  
@@ -32,7 +46,12 @@ namespace PL4 {
     auto it1 = begin;
     auto it2 = begin + 1;
     while (it2 < end) {
-      if (*it1 > *it2) std::swap(*it1, *it2);
+      if (*it1 < *it2){
+        
+      }
+      else{
+        std::swap(*it2, *it1);
+      } 
       ++it1;
       ++it2;
     }
