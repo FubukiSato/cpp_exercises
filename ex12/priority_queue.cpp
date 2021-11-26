d67e63d17d67028b20d0a278edbfbd3a0802dd06
FubukiSato <satofubuki0510@gmail.com>
2021-11-27 00:05:20 +0900

    edit

--- ex12/priority_queue.cpp
+++ ex12/priority_queue.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+#include <cstdio>
 #include <queue>
 #include <list>
 #include <math.h>
+
+
 
 struct Point {
  Point() : x(0.0), y(0.0), z(0.0) {}
@@ -9,7 +12,17 @@ struct Point {
  double x, y, z;
 };
 
-typedef std::priority_queue<int, std::list<Point> > doublepriority_queue;
+bool operator <(const Point p1, const Point p2){
+    if(sqrt(pow(p1.x,2)+pow(p1.y,2)+pow(p1.z,2)) < sqrt(pow(p2.x,2)+pow(p2.y,2)+pow(p2.z,2))) return true;
+    else return false;
+};
+
+struct Compare {
+    bool operator()(Point p1, Point p2){
+        if(p1 < p2) return true;
+        else return false;
+    }
+};
 
 namespace L12{
 template <class List>
@@ -18,14 +31,8 @@ List find_k_closest(int k,List list){
 }
 }
 
-
-bool Compare(Point p1,Point p2){
-    if(sqrt(pow(p1.x,2)+pow(p1.y,2)+pow(p1.z,2)) < sqrt(pow(p2.x,2)+pow(p2.y,2)+pow(p2.z,2))) return true;
-    else return false;
-}
-
-
 int main(void) {
+    std::priority_queue<Point> doublepriority_queue;
   // COMPLETE 
   // Add code to test your implementation of the function find_k_closest()
   Point p1 = Point(1.0,3.0,5.0);
@@ -34,17 +41,17 @@ int main(void) {
   Point p4 = Point(4.0,1.0,3.0);
   Point p5 = Point(9.0,1.0,1.0);
 
-  doublepriority_queue doublepriority_queue1;
-  doublepriority_queue1.push(p1);
-  doublepriority_queue1.push(p2);
-  doublepriority_queue1.push(p3);
-  doublepriority_queue1.push(p4);
-  doublepriority_queue1.push(p5);
+  doublepriority_queue.push(p1);
+  doublepriority_queue.push(p2); 
+  doublepriority_queue.push(p3);
+  doublepriority_queue.push(p4);
+  doublepriority_queue.push(p5);
 
-  L12::find_k_closest(3,doublepriority_queue1);
+  /*L12::find_k_closest(3,doublepriority_queue);*/
 
-  while(!doublepriority_queue1.empty()){
-      std::cout<<doublepriority_queue1.top().x << doublepriority_queue1.top().y << doublepriority_queue1.top().z <<std::endl;
+  while(!doublepriority_queue.empty()){
+      std::cout<<doublepriority_queue.top().x<< " " << doublepriority_queue.top().y << " " << doublepriority_queue.top().z <<std::endl;
+      doublepriority_queue.pop();
   }
 
   return 0;
