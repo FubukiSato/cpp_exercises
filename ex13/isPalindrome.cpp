dd9a3694725a8e7eb6c80fcb0e0291320539acd3
FubukiSato <satofubuki0510@gmail.com>
2021-11-28 14:21:48 +0900

    edit

--- ex13/isPalindrome.cpp
+++ ex13/isPalindrome.cpp
@@ -5,7 +5,27 @@
 
 
 
-bool isPalindrome(){
+bool isPalindrome(std::string s1){
+
+    std::string cpy;
+
+    transform (s1.begin(), s1.end(), s1.begin(), tolower);
+    
+    s1.erase(std::remove_if(s1.begin(), s1.end(), ispunct), s1.end());
+
+    s1.erase(std::remove(s1.begin(), s1.end(), ' '), s1.end());
+
+    cpy = s1;
+
+    std::reverse(cpy.begin(),cpy.end());
+
+    if(s1 == cpy){
+        return true;
+    }else{
+        return false;
+    }
+
+    
     return true;
 }
 
@@ -14,10 +34,16 @@ int main(){
     std::string s1("Don't nod");
     std::string s2("I am hero");
 
-    transform (s1.begin(), s1.end(), s1.begin(), tolower);
-    transform (s2.begin(), s2.end(), s2.begin(), tolower);
+    if(isPalindrome(s1) == true){
+        std::cout << "this is Palindrome" << std::endl;
+    }else{
+        std::cout << "this is not Palindrome" << std::endl;
+    }
 
-    std::cout << s1 << std::endl;
-    std::cout << s2 << std::endl;
+    if(isPalindrome(s2) == true){
+        std::cout << "this is Palindrome" << std::endl;
+    }else{
+        std::cout << "this is not Palindrome" << std::endl;
+    }
     return 0;
 }
